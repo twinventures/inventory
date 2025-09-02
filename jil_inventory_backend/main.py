460e8a0794ed1ad97e5b9a675e372024e70f9cf7
mutakazi <116642751+twinventures@users.noreply.github.com>
2025-09-01 20:39:52 -0400

    It finally worked, just added the prod. URL to see if it'll still work - this tightens security.

--- jil_inventory_backend/main.py
+++ jil_inventory_backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://inventory-web-demo.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=False,
